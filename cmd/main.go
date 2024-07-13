1f46c1a7396ebed163c26ded732f447a4bc762f8
Sasha <sgenof@gmail.com>
2024-07-13 11:44:08 +0300

    first commit

---
+++ cmd/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Yandex-Practicum/go-first-floor-sprint-four/internal/ftracker"
+)
+
+func main() {
+	fmt.Println(ftracker.ShowTrainingInfo(4000, "Бег", 2.0, 85, 185, 50, 5))
+	fmt.Println(ftracker.ShowTrainingInfo(4000, "Ходьба", 1, 85, 185, 50, 5))
+	fmt.Println(ftracker.ShowTrainingInfo(1000, "Плавание", 0.25, 85, 185, 100, 4))
+	fmt.Println(ftracker.ShowTrainingInfo(1000, "Керлинг", 5, 85, 185, 50, 2))
+
+}
